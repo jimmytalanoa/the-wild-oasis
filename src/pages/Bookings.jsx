a31632b0541f3c9e9f903682130bcb6acdf129f8
jimmytalanoa <jimmytalanoa@gmail.com>
2024-06-14 13:52:41 +1000

    refresh data

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -2,6 +2,7 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import BookingTable from "../features/bookings/BookingTable";
 import BookingTableOperations from "../features/bookings/BookingTableOperations";
+import Uploader from "../data/Uploader";
 
 function Bookings() {
   return (
